e8dee727bf2b9d0434d0b75a6870d84d00a9a4b9
Stefanus Andre <stefanus@Stefanuss-MacBook-Air.local>
2024-08-04 04:40:30 +0700

    belajar imageviewer

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import ImageViewer from './imageViewer';
+import Button from './Button';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+        {/* <Image source={PlaceholderImage} style={styles.image}/> */}
+        {/* <Text style={{color: 'blue'}}>Open up App.js to start working on your app!</Text> */}
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58
+  },
+  image : {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
